676191e95a8aff14cdd77b67c385436b0e1b0cee
俊华 蔡 <1032031912@qq.com>
2020-09-16 14:23:41 +0800

    1.Ref参数 2.out参数

---
+++ 07_Ref参数/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Ref参数
+{
+    class Test
+    {
+        public static void Swap(ref int x,ref int y)//两个数进行交换
+        {
+            Console.WriteLine("[Swap]  Before x={0},y={1}", x, y);
+            int temp = x;//中间变量
+            x = y;
+            y = temp;
+            Console.WriteLine("[Swap]  After x={0},y={1}", x, y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 2;
+            Console.WriteLine("Before swap,i={0},j={1}", i, j);
+            Test.Swap(ref i,ref j);
+            Console.WriteLine("After swap,i={0},j={1}", i, j);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 08_out输出参数/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_out输出参数
+{
+
+    class TEST
+    {
+        public static bool MyTryParse(string str,out int Number)
+        {
+            bool result;
+            try
+            {
+                Number = Convert.ToInt32(str);
+                result = true;
+            }
+            catch (Exception)
+            {
+                Number = 0;
+                result = false;
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "123";
+            int n;
+            if (TEST.MyTryParse(s, out n))
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine("不能转换成整数");
+            }
+            Console.ReadKey();
+        }
+    }
+}
