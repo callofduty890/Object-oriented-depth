ae59f2c38cbeeef0d483bd5833eface1a4ff664c
俊华 蔡 <1032031912@qq.com>
2020-09-16 10:56:36 +0800

    1.添加访问修饰符 2.添加了常量

---
+++ 02_访问修饰符/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_访问修饰符
+{
+    class Person
+    {
+        public const int PRETIREMENT_AGE = 65;//访问不受限
+        public string name;//访问不受限
+        internal string nickName;//在当前程序内可以访问
+        protected bool isMarried;//在person类或派生类中可以访问
+        private int age;//只能在person类中访问
+        string creditiCarNum;//使用默认访问修饰符:Private。只能在类中可以访问
+        public void Speak() //访问不受限
+        {
+            Console.WriteLine("Hi");
+        }
+
+        public void Mothod1()
+        {
+            //对当前类的成员都可以访问
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pr = new Person();
+            //对程序的类中的方法，对象person成员的访问权限如下
+            //publi和internal成员都可以访问；Protected和Private成员不可访问
+
+        }
+    }
+}
---
+++ 03_常量与字段/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_常量与字段
+{
+    class person
+    {
+        public const int RETIREMENT_AGE = 60;//声明了一个常量
+        public const int RETIREMENT_AGE_DELAY = RETIREMENT_AGE + 60;
+        string name;int age;//声明了两个变量
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //调用显示常量
+            Console.WriteLine("AGE={0},AGE_DELAY={1}", person.RETIREMENT_AGE, person.RETIREMENT_AGE_DELAY);
+            
+            Console.ReadKey();
+        }
+    }
+}
