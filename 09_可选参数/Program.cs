a7a9037d8260620e9ace6a9824e9175faf396f97
俊华 蔡 <1032031912@qq.com>
2020-09-16 14:48:52 +0800

    1.可选参数 2.可变参数

---
+++ 09_可选参数/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_可选参数
+{
+    class Program
+    {
+        public static void Display(int a,int b=2,int c=3)
+        {
+            Console.WriteLine("调用有三个参数的方法");
+            Console.WriteLine("a={0},b={1},c={2}", a, b, c);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Display(1);//a使用传入的参数，b与c使用默认的值
+            Display(1, 3);
+            Display(1, 3, 5);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 10_可变参数_Params/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_可变参数_Params
+{
+    class Program
+    {
+        static void F(params int[] args)
+        {
+            Console.Write("数组包含了{0}个元素", args.Length);
+            foreach (int item in args)Console.Write("{0}\t", item);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            F(arr);
+            F(10, 20, 30, 40);
+            F();
+            Console.ReadKey();
+        }
+    }
+}
