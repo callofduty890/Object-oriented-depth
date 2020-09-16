b5076d8db08f451f70fe6f8798b1d3d69844164f
俊华 蔡 <1032031912@qq.com>
2020-09-16 09:48:30 +0800

    this关键字的使用

---
+++ 01_this关键字/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_this关键字
+{
+    class ThisTest
+    {
+        public int i1 = 123;//声明实例字段并赋初值
+        public string s1 = "abc";//声明实例字段并赋初值
+        public ThisTest(int i1,string s1)
+        {
+            //i1 = i1;//变量赋值无意义：左边的是i1局部变量，没有赋值
+            //s1 = s1;//变量赋值无意义：左边的是s1局部变量，没有赋值
+            this.i1 = i1;   //正确
+            this.s1 = s1; //正确
+        }
+
+        public void printInfo() //实例方法
+        {
+            int i1 = 456; //声明了局部变量，与实例字段重名
+            string s1 = "def";//声明了局部变量，与实例字段重名
+            Console.WriteLine(i1 + " " + s1);//输出局部变量
+            Console.WriteLine(this.i1 + " " + this.s1);//输出实例字段
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThisTest obj1 = new ThisTest(999, "AAA");
+            obj1.printInfo();
+            Console.ReadKey();
+        }
+    }
+}
--- 9_16_类与对象/Program.cs
+++ 9_16_类与对象/Program.cs
@@ -44,8 +44,8 @@ namespace _9_16_类与对象
             c1.ShowInStance();    //正确：对象调用方法
             //c1.ShowStatic();        //错误:不能用对象调用静态的方法
             Console.Write("object:{0}",c1.number);//正确：用对象引用实例字段
-            Console.Write("object:{0}", Counter.number);//错误：不能用类名引用实例字段
-            Console.Write("count:{0}", c1.count);//错误：不能用对象名引用静态字段
+            //Console.Write("object:{0}", Counter.number);//错误：不能用类名引用实例字段
+            //Console.Write("count:{0}", c1.count);//错误：不能用对象名引用静态字段
             Console.Write("count:{0}", Counter.count);//正确:用类名调用静态字段
 
 
