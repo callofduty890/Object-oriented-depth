26d11d70691fe1cad4db0ae8f01a81a758e46b3b
俊华 蔡 <1032031912@qq.com>
2020-09-16 11:30:03 +0800

    添加只读字段 添加方法

---
+++ 04_只读字段/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_只读字段
+{
+    class ReadOnlyField
+    {
+        public int x;
+        public readonly int y = 2;//声明并初始化只读字段
+        public readonly int z;      //声明只读字段
+        public ReadOnlyField()
+        {
+            y = 6;
+            z = 3;//初始化只读字段
+        }
+        public ReadOnlyField(int p1, int p2, int p3)
+        {
+            this.x = p1;this.y = p2;this.z = p3;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnlyField p1 = new ReadOnlyField();//无参
+            p1.x = 1;//正确
+            //p1.z = 33;//错误：编译错误对只读字段赋值
+            Console.WriteLine("p1:x={0}, y={1}, z{2}", p1.x, p1.y, p1.z);
+
+            ReadOnlyField p2 = new ReadOnlyField(11, 22, 33);
+            Console.WriteLine("p2:x={0}, y={1}, z{2}", p2.x, p2.y, p2.z);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 05_方法/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_方法
+{
+    class SimpleMath
+    {
+        public int AddTwoNumbers(int Number1, int Number2)//两数相加
+        {
+            return Number1 + Number2;
+        }
+
+        public int SquareANumber(int Number)//求某数的平方
+        {
+            return Number * Number;
+        }
+
+        public static void DisplayResult(int number)//显示结果
+        {
+            Console.WriteLine("结果为:{0}", number);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result; SimpleMath obj = new SimpleMath();
+            result = obj.AddTwoNumbers(1, 2);//两个数相机
+            SimpleMath.DisplayResult(result);//显示相加的结果
+            SimpleMath.DisplayResult(obj.SquareANumber(result));//计算平方
+
+            Console.ReadKey();
+        }
+    }
+}
